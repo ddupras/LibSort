9cfb5778b67a2a69e7d0db3f2baaf1cf4de99e54
ddupras <derek.dupras@gmail.com>
2017-03-28 21:20:16 -0700

    Move test code to separate file.

--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -1,63 +1,7 @@
 // merge_sort.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
-
-#include <stdio.h>
-#include <iostream>
 #include <vector>
-#include <climits>
-
-void DumpVector (std::vector<int> &A);
-void MergeSort (std::vector<int> &A, int p, int r);
-void Merge (std::vector<int> &A, int p, int q, int r);
-void BubbleSort (std::vector<int> &A);
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	int A_values[] = {3, 41, 52, 26, 38, 57, 9, 49};
-	std::vector<int> A (A_values, A_values + sizeof(A_values) / sizeof(int) );
-
-	std::cout << "Merge Sort" << std::endl;
-
-	DumpVector(A);
-
-	std::cout << "Sorting " << A.size() << " elements." << std::endl;
-
-	//MergeSort(A, 0, A.size() - 1);
-	BubbleSort(A);
-
-	DumpVector(A);
-
-	std::cout << "Done." << std::endl;
-	getchar();
-
-	return 0;
-}
-
-void DumpVector (std::vector<int> &A)
-{
-	for (std::vector<int>::iterator it = A.begin(); it != A.end(); ++it)
-	{
-		std::cout << ' ' << *it;
-	}
-	std::cout << std::endl;
-}
-
-void MergeSort (std::vector<int> &A, int p, int r)
-{
-	if (p < r)
-	{
-		int q = (p + r) / 2;
-		std::cout << "Sorting " << p << " to " << q << std::endl;
-		MergeSort(A, p, q);
-		std::cout << "Sorting " << q+1 << " to " << r << std::endl;
-		MergeSort(A, q + 1, r);
-		std::cout << "Merging " << p << ", " << q << ", " << r << std::endl;
-		Merge(A, p, q, r);
-		DumpVector(A);
-	}
-}
+//#include <climits>
 
 void Merge (std::vector<int> &A, int p, int q, int r)
 {
@@ -79,7 +23,6 @@ void Merge (std::vector<int> &A, int p, int q, int r)
 	int j = 0;
 	for (int k = p; k <= r; k++)
 	{
-		DumpVector(A);
 		if (L[i] <= R[j])
 		{
 			A[k] = L[i];
@@ -91,18 +34,13 @@ void Merge (std::vector<int> &A, int p, int q, int r)
 	}
 }
 
-void BubbleSort (std::vector<int> &A)
+void MergeSort(std::vector<int> &A, int p, int r)
 {
-	for (int i = 0; i < A.size() - 2; i++)
+	if (p < r)
 	{
-		for (int j = A.size() - 1; j > i + 1; j--)
-		{
-			if (A[j] < A[j - 1])
-			{
-				int temp = A[j];
-				A[j] = A[j - 1];
-				A[j - 1] = temp;
-			}
-		}
+		int q = (p + r) / 2;
+		MergeSort(A, p, q);
+		MergeSort(A, q + 1, r);
+		Merge(A, p, q, r);
 	}
 }
---
+++ merge_sort/test.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+void DumpVector(std::vector<int> &A);
+void MergeSort(std::vector<int> &A, int p, int r);
+void Merge(std::vector<int> &A, int p, int q, int r);
+void BubbleSort(std::vector<int> &A);
+
+int main(int argc, char* argv[])
+{
+	int A_values[] = { 3, 41, 52, 26, 38, 57, 9, 49 };
+	std::vector<int> A(A_values, A_values + sizeof(A_values) / sizeof(int));
+
+	std::cout << "Merge Sort" << std::endl;
+
+	DumpVector(A);
+
+	std::cout << "Sorting " << A.size() << " elements." << std::endl;
+
+	MergeSort(A, 0, A.size() - 1);
+
+	DumpVector(A);
+
+	std::cout << "Done." << std::endl;
+	getchar();
+
+	return 0;
+}
+
+//BubbleSort(A);
+void BubbleSort(std::vector<int> &A)
+{
+	for (size_t i = 0; i < A.size() - 2; i++)
+	{
+		for (size_t j = A.size() - 1; j > i + 1; j--)
+		{
+			if (A[j] < A[j - 1])
+			{
+				int temp = A[j];
+				A[j] = A[j - 1];
+				A[j - 1] = temp;
+			}
+		}
+	}
+}
+
+void DumpVector(std::vector<int> &A)
+{
+	for (std::vector<int>::iterator it = A.begin(); it != A.end(); ++it)
+	{
+		std::cout << ' ' << *it;
+	}
+	std::cout << std::endl;
+}
+
