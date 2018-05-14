c2b32d88e31f45bdd8fec4666b6821f2fc1fb2c4
Derek Dupras <derek.dupras@gmail.com>
2018-05-13 21:13:50 -0700

    Removing unused <string> header and adding #pragma once.

--- include/BubbleSort.h
+++ include/BubbleSort.h
@@ -28,8 +28,9 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+#pragma once
+
 #include <ctime>
-#include <string>
 #include <vector>
 
 #include <LibSort.h>
