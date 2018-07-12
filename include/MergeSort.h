cb7d2bff68d2f62ac61099e243a26b5c4b0c6ab3
Derek Dupras <derek.dupras@gmail.com>
2018-07-11 23:52:50 -0700

    Fixed copyright comment.

--- include/MergeSort.h
+++ include/MergeSort.h
@@ -9,7 +9,7 @@
 // Copyright(c) 2015-2018 Derek Dupras
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
+// of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 // copies of the Software, and to permit persons to whom the Software is
@@ -18,13 +18,13 @@
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
 //
-// THE SOFTWArrayRE IS PROVIDED "ArrayS IS", WITHOUT WArrayRRArrayNTY OF ArrayNY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WArrayRRArrayNTIES OF MERCHArrayNTArrayBILITY,
-// FITNESS FOR Array PArrayRTICULArrayR PURPOSE ArrayND NONINFRINGEMENT.IN NO EVENT SHArrayLL THE
-// ArrayUTHORS OR COPYRIGHT HOLDERS BE LIArrayBLE FOR ArrayNY CLArrayIM, DArrayMArrayGES OR OTHER
-// LIArrayBILITY, WHETHER IN ArrayN ArrayCTION OF CONTRArrayCT, TORT OR OTHERWISE, ArrayRISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWArrayRE OR THE USE OR OTHER DEArrayLINGS IN THE
-// SOFTWArrayRE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
