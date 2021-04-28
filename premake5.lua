160e5662c5bd38577e98c141ea0e802a96d8dc84
Derek Dupras <derek.dupras@gmail.com>
2021-04-27 21:23:29 -0700

    Added LibSort project in premake script
    * Added LibSort project in premake script
    * Fixed deprecation warning in premake script

--- premake5.lua
+++ premake5.lua
@@ -1,21 +1,36 @@
--- (c) Derek Dupras 2017-2018, All Rights Reserved.
+-- (c) Derek Dupras 2017-2021, All Rights Reserved.
 
 -- premake5.lua
 workspace "LibSort"
-   configurations { "Debug", "Release" }
-   location "build"
+	configurations { "Debug", "Release" }
+	location "build"
+
+project 'LibSort'
+	kind 'None'
+	language "C++"
+	files { 
+		'include/**',
+		'.clang-format',
+		'premake5.lua'
+	}
+	vpaths {
+		['Headers/**'] = { 'include/**.h', 'include/**.hpp' },
+		['Clang/**'] = { '.clang-format' },
+		['Premake/**'] = { 'premake5.lua' },
+		['Docs'] = '**.md'
+	}
 
 project "LibSort-test"
-   location "build"
-   kind "ConsoleApp"
-   language "C++"
+	location "build"
+	kind "ConsoleApp"
+	language "C++"
 
-   -- Catch will define a main if not provided, but we need to define DO_NOT_USE_WMAIN
-   defines { "DO_NOT_USE_WMAIN" }
+	-- Catch will define a main if not provided, but we need to define DO_NOT_USE_WMAIN
+	defines { "DO_NOT_USE_WMAIN" }
 
-   targetdir "bin/%{cfg.buildcfg}"
-   includedirs { "include", "external/catch" }
-   files { "**.h", "**.cpp" }
+	targetdir "bin/%{cfg.buildcfg}"
+	includedirs { "include", "external/catch" }
+	files { "**.h", "**.cpp" }
 
 	configuration "Debug"
 		defines { "DEBUG" }
@@ -23,4 +38,4 @@ project "LibSort-test"
 
 	configuration "Release"
 		defines { "NDEBUG" }
-		flags { "Optimize" }
+		optimize "On"
